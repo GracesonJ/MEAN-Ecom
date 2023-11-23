94eaeaa83ec00e3eab03e7b63b7db8578b4ed469
Graceson j <gracesonjjohnson@gmail.com>
2023-11-23 10:55:24 +0530

    header created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // all products
+  {
+    path:'', component:AllProductsComponent
+  },
+  // login
+  {
+    path:'user/login', component:LoginComponent
+  },
+  // register
+  {
+    path:'user/register', component:RegisterComponent
+  },
+  // wishlist
+  {
+    path:'wishlist', component:WishlistComponent
+  },
+  // cart
+  {
+    path:'cart', component:CartComponent
+  },
+  // checkout
+  {
+    path:'user/checkout',component:CheckoutComponent
+  },
+  // view products
+  {
+    path:'product/view:id', component:ViewProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AllProductsComponent,
+    WishlistComponent,
+    CartComponent,
+    CheckoutComponent,
+    LoginComponent,
+    RegisterComponent,
+    ViewProductComponent
   ],
   imports: [
     BrowserModule,
